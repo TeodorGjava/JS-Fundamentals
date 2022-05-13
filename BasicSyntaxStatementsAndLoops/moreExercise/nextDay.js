f4cc21bf5dcfe840b9942712888e9093a942e24e
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-05-13 19:51:10 +0300

    Update nextDay.js

--- BasicSyntaxStatementsAndLoops/moreExercise/nextDay.js
+++ BasicSyntaxStatementsAndLoops/moreExercise/nextDay.js
@@ -1,5 +1,12 @@
 function recompile(a,b,c){
+    if(c==30){
+        b++;
+        c=0;
+        c++;
+    }else{
+        c++;
+    }
     var date = a+"-"+b+"-"+c;
     console.log(date);
 }
-recompile(2022, 6, 4);
+recompile(2016, 6, 30);
